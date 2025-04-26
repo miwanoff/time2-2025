af96077f254dae57e7dd3ffcc10f3ef2f0bef466
miwanoff <marina.iva.noffa@gmail.com>
2025-04-26 10:47:17 +0300

    Add years

--- age.js
+++ age.js
@@ -1,15 +1,17 @@
 function birthDateToAge(birth) {
-    let n = new Date();
-    let b = new Date(birth);
-        console.log(n.getFullYear());
-        console.log(b.getFullYear());
-        age = n.getFullYear() - b.getFullYear();
-        let n_2000 = n.setFullYear(2000); // приводимо дату до 2000 року
-        let b_2000 = b.setFullYear(2000); //приводимо день рождения до 2000 року        
-    // якщо дата менше, ніж день народження (дня народження ще не було), віднімаємо 1
-    if (n_2000 < b_2000)
-         age -= 1;
-     return age;
-} 
+  let n = new Date();
+  let b = new Date(birth);
+  console.log(n.getFullYear());
+  console.log(b.getFullYear());
+  age = n.getFullYear() - b.getFullYear();
+  console.log(age);
+  let n_2025 = n.setFullYear(2025); // приводимо дату до 2025року
+  let b_2025 = b.setFullYear(2025); //приводимо день рождения до 2025 року
+  // якщо дата менше, ніж день народження (дня народження ще не було), віднімаємо 1
+  if (n_2025 < b_2025) age -= 1;
+  console.log(age % 10);
+  let years = "років";
+  return `${age} ${years}`;
+}
 
-alert(birthDateToAge("2004-07-15"));
+alert(birthDateToAge("2001-02-15"));
