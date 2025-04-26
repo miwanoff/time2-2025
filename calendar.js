b8449ee0c64c6e9568f414fdea8021ebf6efcfb2
miwanoff <marina.iva.noffa@gmail.com>
2025-04-26 11:42:33 +0300

    Add lastDate

--- calendar.js
+++ calendar.js
@@ -1,18 +1,27 @@
 const week = ["НД", "ПН", "ВТ", "СР", "ЧТ", "ПТ", "СБ"];
 let d = new Date();
-// let d = new Date("2025-04-27");
+// let d = new Date("2025-03-28");
 let today = d.getDate();
 let month = d.getMonth();
 let year = d.getFullYear();
 let dayOfWeek = d.getDay();
-document.writeln(`<h2>Сьогодні ${today}.${month + 1}.${year}, ${week[dayOfWeek]}</h2>`);
 
-let dateOfMon = 1; 
-if(dayOfWeek != 0){
+let f = new Date(year, month + 1, 1);
+//alert(f);
+let last = new Date(f - 1);
+
+let lastDate = last.getDate();
+console.log(lastDate);
+
+document.writeln(
+  `<h2>Сьогодні ${today}.${month + 1}.${year}, ${week[dayOfWeek]}</h2>`
+);
+
+let dateOfMon = 1;
+if (dayOfWeek != 0) {
   dateOfMon = today - (dayOfWeek - 1);
-}
-else {
-    dateOfMon = today - 6;
+} else {
+  dateOfMon = today - 6;
 }
 console.log(dateOfMon);
 
@@ -29,7 +38,7 @@ document.writeln("</tr>");
 document.writeln("<tr>");
 for (let i = 0; i < week.length; i++) {
   document.writeln("<td>");
-  document.writeln(dateOfMon + i);
+  document.writeln(`<span class="today">${dateOfMon + i}</span>`);
   document.writeln("</td>");
 }
 document.writeln("</tr>");
