90976c19e7d5705db5ce13e8d2e0659d83f540fb
miwanoff <marina.iva.noffa@gmail.com>
2025-04-26 10:56:24 +0300

    Add last == 1

--- age.js
+++ age.js
@@ -9,9 +9,16 @@ function birthDateToAge(birth) {
   let b_2025 = b.setFullYear(2025); //приводимо день рождения до 2025 року
   // якщо дата менше, ніж день народження (дня народження ще не було), віднімаємо 1
   if (n_2025 < b_2025) age -= 1;
-  console.log(age % 10);
+  let last = age % 10;
+  console.log(last);
   let years = "років";
+  if (age < 11 || age > 14) {
+    if (last == 1) {
+      years = "рік";
+    }
+    /* доробити для останніх  цифр 2, 3, 4 роки */
+  }
   return `${age} ${years}`;
 }
 
-alert(birthDateToAge("2001-02-15"));
+alert(birthDateToAge("2014-02-15"));
